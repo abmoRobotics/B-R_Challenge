7b8fdb607641d5cd16a1d36641ac6403d6f9a589
abmoRobotics <abmoRobotics@student.aau.dk>
2023-03-22 20:57:03 +0100

    Added tests

--- example_program/py/estimator/problem/test_python.py
+++ example_program/py/estimator/problem/test_python.py
@@ -1,155 +1,140 @@
+from LayoutGraphOptimizer.LayoutGraph import LayoutGraph, Path
 import time
-
-# def product(*iterables):
-#     if not iterables:
-#         return [[]]
-#     else:
-#         results = []
-#         for item in iterables[0]:
-#             for sub_product in product(*iterables[1:]):
-#                 results.append([item] + sub_product)
-#         return results
+from LayoutGraphOptimizer.utils import *
+# from find_paths import LAYOUT, WEIGHTS, VARIANT_MIXES, COLOR_MAP
+# print(LAYOUT)
 
 
+def test_combinatorial_time():
+    LAYOUT = {
+    'nodes': [
+        'b', 'null','y','null','y','null','g',
+        'y', 'null', 'y','null','null','null', 'y',
+        'null','null','null','null','null','null','g',
+        'null','null','null','null','null','null','b',
+        'g', 'null','g','null', 'null','null','null',
+        'g', 'null','g','null', 'b','null','null'
+    ],
+    'length_x': 7,
+    'length_y': 6
+    }
+
+    # Define weights (i.e. processing time) for each node type (including null)
+    WEIGHTS = {
+        'b': 3,
+        'y': 3,
+        'g': 5,
+        'null': 1,
+    }
+
+    # Set colors for nodes, which is needed for plotting the graph. Note that each you need to define the color for each 
+    #   node type (including null) as well as the 'start' and 'end' node types.
+    COLOR_MAP = {
+        'start': 'black',
+        'b': 'blue',
+        'y': 'darkgoldenrod',
+        'g': 'green',
+        'end': 'black',
+        'null': 'grey'
+    }
+
+    # Variant Mix A: How many of each type of node should be traversed?
+    VARIANT_MIXES = {
+        'mix_a': {
+            'g': 4,
+            'y': 1,
+            'b': 1
+        },
+        'mix_b': {
+            'g': 0,
+            'y': 0,
+            'b': 1
+        },
+        'mix_c': {
+            'g': 1,
+            'y': 0,
+            'b': 1
+        },
+        'mix_d': {
+            'g': 0,
+            'y': 1,
+            'b': 1
+        }
+    }
 
 
+    graphGlobal = LayoutGraph(LAYOUT, WEIGHTS)
 
-# # Example usage:
-# iterables = [[(0, 0), (6, 3), (4, 5)], [(6, 0), (6, 2), (0, 4), (2, 4), (0, 5), (2, 5)], [(6, 0), (6, 2), (0, 4), (2, 4), (0, 5), (2, 5)]]
-# a = product(*iterables)
-# for b in a:
-#     print(b)
+    # Calculate the best combinations of stations for the different mixes
+    combinations = {}
+    combinations_new_k4 = {}
+    combinations_new_k3 = {}
+    combinations_new_k2 = {}
+    combinations_new_k1 = {} 
 
-# print(f'length: {len(a)}')
+    start_time_combinations_old = time.perf_counter()
+    for mix_type in VARIANT_MIXES:
+        # Obtaining all the valid combinations and corresponding cost
+        combinations[mix_type] = graphGlobal.get_all_valid_combinations(VARIANT_MIXES[mix_type])
+    total_time_combinations_old = time.perf_counter() - start_time_combinations_old
+
+    start_time_combinations_new_k4 = time.perf_counter()
+    for mix_type in VARIANT_MIXES:
+        combinations_new_k4[mix_type] = graphGlobal.get_all_valid_combinations_new(VARIANT_MIXES[mix_type], k=4)
+    total_time_combinations_new_k4 = time.perf_counter() - start_time_combinations_new_k4
+
+    start_time_combinations_new_k3 = time.perf_counter()
+    for mix_type in VARIANT_MIXES:
+        combinations_new_k3[mix_type] = graphGlobal.get_all_valid_combinations_new(VARIANT_MIXES[mix_type], k=3)
+    total_time_combinations_new_k3 = time.perf_counter() - start_time_combinations_new_k3
+
+    start_time_combinations_new_k2 = time.perf_counter()
+    for mix_type in VARIANT_MIXES:
+        combinations_new_k2[mix_type] = graphGlobal.get_all_valid_combinations_new(VARIANT_MIXES[mix_type], k=2)
+    total_time_combinations_new_k2 = time.perf_counter() - start_time_combinations_new_k2
+
+    start_time_combinations_new_k1 = time.perf_counter()
+    for mix_type in VARIANT_MIXES:
+        combinations_new_k1[mix_type] = graphGlobal.get_all_valid_combinations_new(VARIANT_MIXES[mix_type], k=1)
+    total_time_combinations_new_k1 = time.perf_counter() - start_time_combinations_new_k1
+
+    best_combination = get_best_combination(combinations['mix_a'])
+    worst_combination = get_worst_combination(combinations['mix_a'])
+
+    best_combination_new_k4 = get_best_combination(combinations_new_k4['mix_a'])
+    worst_combination_new_k4 = get_worst_combination(combinations_new_k4['mix_a'])
+
+    best_combination_new_k3 = get_best_combination(combinations_new_k3['mix_a'])
+    worst_combination_new_k3 = get_worst_combination(combinations_new_k3['mix_a'])
+
+    best_combination_new_k2 = get_best_combination(combinations_new_k2['mix_a'])
+    worst_combination_new_k2 = get_worst_combination(combinations_new_k2['mix_a'])
+
+    best_combination_new_k1 = get_best_combination(combinations_new_k1['mix_a'])
+    worst_combination_new_k1 = get_worst_combination(combinations_new_k1['mix_a'])
+
+    print(f"<--------------------------------------------------------------------------------->")
+    print('INFO FOR MIX A')
+    print(f'Number of stations in mix A:        {VARIANT_MIXES["mix_a"]["g"] + VARIANT_MIXES["mix_a"]["y"] + VARIANT_MIXES["mix_a"]["b"]}')
+    print("<---------------------------------------------------------------------------------->")
+    print(f'{"Best combination cost":37} {best_combination.cost:<7} {"number of combinations"} {len(combinations["mix_a"]):>7}')
+    print(f'{"best_combination_new k=4 cost":37} {best_combination_new_k4.cost:<7} {"number of combinations"} {len(combinations_new_k4["mix_a"]):>7}')
+    print(f'{"best_combination_new k=3 cost":37} {best_combination_new_k3.cost:<7} {"number of combinations"} {len(combinations_new_k3["mix_a"]):>7}')
+    print(f'{"best_combination_new k=2 cost":37} {best_combination_new_k2.cost:<7} {"number of combinations"} {len(combinations_new_k2["mix_a"]):>7}')
+    print(f'{"best_combination_new k=1 cost":37} {best_combination_new_k1.cost:<7} {"number of combinations"} {len(combinations_new_k1["mix_a"]):>7}')
+    print("<---------------------------------------------------------------------------------->")
+    print(f'{"best_combination_old nodes":37} {best_combination.nodes}') 
+    print(f'{"best_combination_new k=4 nodes":37} {best_combination_new_k4.nodes}')
+    print(f'{"best_combination_new k=3 nodes":37} {best_combination_new_k3.nodes}')
+    print(f'{"best_combination_new k=2 nodes":37} {best_combination_new_k2.nodes}')
+    print(f'{"best_combination_new k=1 nodes":37} {best_combination_new_k1.nodes}')
+    print("<---------------------------------------------------------------------------------->")
+    print(f'{"Execution time combinations_old":37} {total_time_combinations_old:.2f}s')
+    print(f'{"Execution time combinations_new k=4":37} {total_time_combinations_new_k4:.2f}s')
+    print(f'{"Execution time combinations_new k=3":37} {total_time_combinations_new_k3:.2f}s')
+    print(f'{"Execution time combinations_new k=2":37} {total_time_combinations_new_k2:.2f}s')
+    print(f'{"Execution time combinations_new k=1":37} {total_time_combinations_new_k1:.2f}s')
 
 
-import math
-
-def euclidean_distance(point1, point2):
-    return math.sqrt(sum((p1 - p2)**2 for p1, p2 in zip(point1, point2)))
-
-def product2(distance_threshold=None, *iterables):
-    if not iterables:
-        return [[]]
-    else:
-        results = []
-        for item in iterables[0]:
-
-            for sub_product in product2(distance_threshold, *iterables[1:]):
-                combination = [item] + sub_product
-                
-                if distance_threshold is None:
-                    results.append(combination)
-                else:
-
-                    distances = [euclidean_distance(combination[i], combination[i + 1]) for i in range(len(combination) - 1)]
-                    if all(distance <= distance_threshold for distance in distances):
-                        results.append(combination)
-        return results
-
-# Example usage:
-iterables = [[(0, 0), (6, 3), (4, 5)], [(6, 0), (6, 2), (0, 4), (2, 4), (0, 5), (2, 5)], [(6, 0), (6, 2), (0, 4), (2, 4), (0, 5), (2, 5)]]
-distance_threshold = 3  # You can change this value to adjust the threshold
-paths = product2(distance_threshold, *iterables)
-# for path in paths:
-#     print(path)
-
-# print(f'length: {len(paths)}')
-
-
-# import heapq
-
-# def knn(point, points, k):
-#     distances = [(euclidean_distance(point, other_point), other_point) for other_point in points]
-#     heapq.heapify(distances)
-#     return [heapq.heappop(distances)[1] for _ in range(min(k, len(distances)))]
-
-# def product3(k=None, *iterables):
-#     if not iterables:
-#         return [[]]
-#     else:
-#         results = []
-#         for item in iterables[0]:
-#             neighbors = knn(item, iterables[1], k) if k is not None else iterables[1]
-#             for sub_product in product3(k, *([neighbors] + iterables[2:])):
-#                 results.append([item] + sub_product)
-#         return results
-
-# # Example usage:
-# iterables = [[(0, 0), (6, 3), (4, 5)], [(6, 0), (6, 2), (0, 4), (2, 4), (0, 5), (2, 5)], [(6, 0), (6, 2), (0, 4), (2, 4), (0, 5), (2, 5)]]
-# k = 3  # You can change this value to adjust the number of neighbors
-# print(product3(k, *iterables))
-
-
-
-import heapq
-import math
-
-# This function is used to calculate the Euclidean distance between two points
-def euclidean_distance(point1, point2):
-    """ Returns the Euclidean distance between two points. """
-    return math.sqrt(sum((p1 - p2) ** 2 for p1, p2 in zip(point1, point2)))
-
-def knn(point, points, k):
-    """ Returns the k nearest neighbors of a point.
-    
-    Args:
-        point: The point for which we want to find the k nearest neighbors.
-        points: The list of points from which we want to find the k nearest neighbors.
-        k: The number of nearest neighbors to find.
-        
-    Returns:
-        The k nearest neighbors of the point.
-    """
-    # Euclidean distance is our heuristic for finding the nearest neighbors or the "best/cheapest" next step, so we use a min-heap.
-    distances = [(euclidean_distance(point, other_point), other_point) for other_point in points] 
-    heapq.heapify(distances)    # This is a min-heap, so the smallest distance will be at the top.
-    return [heapq.heappop(distances)[1] for _ in range(min(k, len(distances)))]# Return the k nearest neighbors
-
-def product3(k=None, *iterables):
-    global a
-    """ Returns the Cartesian product of the iterables.
-    
-    Args:
-        k: The number of nearest neighbors to find for each point. k scales the number of combinations by k^N, where N is the number of iterables.
-        *iterables: The iterables to take the Cartesian product of.
-        
-    Returns:
-        The Cartesian product of the iterables.
-    """
-    if not iterables:
-        return [[]]
-    else:
-        results = []
-        for item in iterables[0]:
-            if len(iterables) > 1:
-                neighbors = knn(item, iterables[1], k) if k is not None else iterables[1]
-                for sub_product in product3(k, *([neighbors] + list(iterables[2:]))):
-                    results.append([item] + sub_product)
-
-            else:
-                results.append([item])
-        return results
-
-# Example usage:
-iterables = [[(0, 0), (6, 3), (4, 5)],
-             [(2, 0), (4, 0), (0, 1), (2, 1), (6, 1)],
-             [(2, 0), (4, 0), (0, 1), (2, 1), (6, 1)], 
-             [(6, 0), (6, 2), (0, 4), (2, 4), (0, 5), (2, 5)], 
-             [(6, 0), (6, 2), (0, 4), (2, 4), (0, 5), (2, 5)], 
-             [(6, 0), (6, 2), (0, 4), (2, 4), (0, 5), (2, 5)], 
-             [(6, 0), (6, 2), (0, 4), (2, 4), (0, 5), (2, 5)], 
-             [(6, 0), (6, 2), (0, 4), (2, 4), (0, 5), (2, 5)], 
-             [(6, 0), (6, 2), (0, 4), (2, 4), (0, 5), (2, 5)], 
-             [(6, 0), (6, 2), (0, 4), (2, 4), (0, 5), (2, 5)]]
-k = 1  # You can change this value to adjust the number of neighbors
-#print(product(k, *iterables))
-print(f'length: {len(product3(k, *iterables))}')
-
-time_start = time.perf_counter()
-paths = product3(k, *iterables)
-print(f'time: {time.perf_counter() - time_start}')
-
-#print(a)
-for path in paths:
-    print(path)
+if __name__ == "__main__":
+    test_combinatorial_time()
